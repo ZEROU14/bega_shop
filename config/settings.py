f8ae42a0abb1429c05775a512aea3a0ee91adb6c
amir <herogame803@gmail.com>
2024-07-15 12:26:05 +0330

    we add some links to the website :)

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,13 @@ DEBUG = env.bool("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True 
+DEFAULT_FROM_EMAIL = "Welcome to our website my man!" 
+EMAIL_HOST_USER =  "herogame803@gmail.com"
+EMAIL_HOST_PASSWORD = "qynpvvlgwgxnwkqc"
 
 # Application definition
 
@@ -151,7 +158,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # accounts config 
 
